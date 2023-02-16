8852ded4e6c401fa933c231abe89d00862dbfbad
s-maarouf <maarouf.salahedd@gmail.com>
2023-02-16 12:48:37 -0800

    task 4

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,10 +1,15 @@
 #include <stdio.h>
 
-int main()
+/**
+* main - A program that prints a line with puts function
+* Return: 0 (success)
+*/
+
+int main(void)
 {
-	char str1[] = "Programming is like building a multilingual puzzle";
+	char str1[] = "\"Programming is like building a multilingual puzzle";
 
 	puts(str1);
 
-	return 0;
+	return(0);
 }
