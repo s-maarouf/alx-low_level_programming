ae3948015095ccba92b071b7390d33cdae3992f5
s-maarouf <maarouf.salahedd@gmail.com>
2023-02-21 01:49:01 -0800

    task 0

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Description: prints _putchar using putchar prototype
+ *
+ * Return: Always 0 (Success)
+*/
+
+int main(void)
+{
+	puts("_putchar\n");
+	return (0);
+}
