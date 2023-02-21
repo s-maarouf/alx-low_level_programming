9c1b91266305630716be8f29da7be1beddb9b3e9
s-maarouf <maarouf.salahedd@gmail.com>
2023-02-21 04:05:22 -0800

    task 2

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -4,18 +4,16 @@
  * print_alphabet_x10 - function that will print the alphabet 10 times
  *
 */
-
 void print_alphabet_x10(void)
 {
 	int l, x;
 
 	l = 0;
 	x = 'a';
-	while (l <= 9)
+	for (l <= 9; l++;)
 	{
 		for (x <= 'z'; x++;)
-			_putchar(x);
-			_putchar('\n');
-		l++;
+			putchar(x);
+		putchar('\n');
 	}
 }
