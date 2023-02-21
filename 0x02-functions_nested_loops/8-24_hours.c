fe66f0ae07330084e08aa70ae93bc89adbb4f5b8
s-maarouf <maarouf.salahedd@gmail.com>
2023-02-21 11:33:58 -0800

    task 8

---
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * jack_bauer- prints every minute of the day
+*/
+
+void jack_bauer(void)
+{
+	int hour, minute;
+	for (hour = 0; hour < 24; hour++)
+	{
+		for (minute = 0; minute < 60; minute++)
+		{
+			printf("%02d:%02d\n", hour, minute);
+		}
+	}
+}
+
