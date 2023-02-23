05b8574f4a4b34f18f5a9161dfa66d712021596c
s-maarouf <maarouf.salahedd@gmail.com>
2023-02-23 13:39:40 -0800

    task 7

---
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -0,0 +1,26 @@
+#include"main.h"
+
+/**
+ * print_line - print a straight line
+ *
+ * @n: is the number of times the _ character
+ *     should be printed
+*/
+
+void print_diagonal(int n)
+{
+	int lnChr, space;
+
+	if (n <= 0)
+		_putchar('\n');
+	else
+	{
+		for (lnChr = 1; lnChr <= n; lnChr++)
+		{
+			for (space = 1; space <= lnChr; space++)
+				_putchar(' ');
+			_putchar(92);
+			_putchar('\n');
+		}
+	}
+}
