31dfa2f8d17a6f730cd99abadf0aed670d68557d
s-maarouf <maarouf.salahedd@gmail.com>
2023-02-23 13:11:19 -0800

    task 4

---
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * print_most_numbers - print 0 - 9 apart
+ *                 from 2 and 4 and you can
+ *                 only use _putchar twice
+ * Return: Always 0 (Success)
+*/
+
+void print_most_numbers(void)
+{
+	int x;
+
+	for (x = 0; x <= 9; x++)
+	{
+		if (x == 2 || x == 4)
+			continue;
+		_putchar(x + 48);
+	}
+	_putchar('\n');
+}
