3535fdd7ab115e3a559da604f723610b8cb05bb7
s-maarouf <maarouf.salahedd@gmail.com>
2023-03-01 14:20:11 -0800

    task 2

---
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ * *_strncpy - is a function that copies the src value to dest
+ *
+ * @dest: is the destionation parameter
+ * @src: is the source parameter to be copied
+ * @n: is the number of characters to be copied
+ *
+ * Return: dest value
+ */
+
+char *_strncpy(char *dest, char *src, int n)
+{
+	int i;
+
+	i = 0;
+	while (i < n && src[i] != '\0')
+	{
+		dest[i] = src[i];
+		i++;
+	}
+	return (dest);
+}
