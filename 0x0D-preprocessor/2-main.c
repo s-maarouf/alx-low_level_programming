751a00de465e15e2f79afd41db90e361d20f9262
s-maarouf <maarouf.salahedd@gmail.com>
2023-03-20 08:06:05 -0700

    completed task 2

---
+++ 0x0D-preprocessor/2-main.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/**
+ * main - is a program that prints the name of the file it was compiled from
+ *
+ * Return: 0
+ */
+
+int main(void)
+{
+	printf(__FILE__"\n");
+	return (0);
+}
