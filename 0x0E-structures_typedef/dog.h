5c65b866526658f444f2fdac314526bc1c7b1d22
s-maarouf <maarouf.salahedd@gmail.com>
2023-03-20 11:35:20 -0700

    Created README file + completed task 0

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,20 @@
+#ifndef dog_h
+#define dog_h
+
+/**
+ * struct dog - a structure named dog
+ *
+ * @name: dog's name
+ * @age: dog's age
+ * @owner: dog's owner
+ *
+ */
+
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+}
+
+#endif
