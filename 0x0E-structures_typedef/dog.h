b2d2d44ed8c3a0b71a9a46cde0a736cb62a9143e
s-maarouf <maarouf.salahedd@gmail.com>
2023-03-20 13:32:47 -0700

    completed task 3

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -17,6 +17,11 @@ struct dog
 	char *owner;
 };
 
+/**
+ * dog_t - a typedef of struct dog
+ */
+
+typedef struct dog dog_t;
 
 void init_dog(struct dog *d, char *name, float age, char *owner);
 void print_dog(struct dog *d);
