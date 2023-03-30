697293309af92e0195ecff1ab5458afbaa4b1915
s-maarouf <maarouf.salahedd@gmail.com>
2023-03-30 16:45:42 -0700

    completed task 1

---
+++ 0x12-singly_linked_lists/1-list_len.c
@@ -0,0 +1,25 @@
+#include "lists.h"
+
+/**
+ * list_len - gets the number of nodes
+ *
+ * @h: pointer to the first node
+ *
+ * Return: number of nodes
+*/
+
+size_t list_len(const list_t *h)
+{
+	size_t node_count = 1;
+
+	if (h == NULL)
+		return (0);
+
+	while (h->next != NULL)
+	{
+		h = h->next;
+		node_count++;
+	}
+
+	return (node_count);
+}
