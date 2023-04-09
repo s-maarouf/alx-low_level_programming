3285d296133e26efd62fac1e64ebdc28875394ed
s-maarouf <maarouf.salahedd@gmail.com>
2023-04-08 18:57:57 -0700

    Completed task 1

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -16,10 +16,6 @@ int create_file(const char *filename, char *text_content)
 	{
 		return (-1);
 	}
-	if (text_content == NULL)
-	{
-		open(filename, O_CREAT | O_RDWR | O_TRUNC, 0600);
-	}
 	while (text_content[i] != '\0')
 	{
 		i++;
