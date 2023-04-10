48042f230d9188971bdb6db78efc07e36870a40a
s-maarouf <maarouf.salahedd@gmail.com>
2023-04-09 18:39:08 -0700

    Completed task 4

--- 0x15-file_io/main.h
+++ 0x15-file_io/main.h
@@ -1,10 +1,13 @@
 #ifndef main_h
 #define main_h
 
-#include <unistd.h>
-#include <stdlib.h>
-#include <stdio.h>
+#include <elf.h>
+#include <sys/types.h>
+#include <sys/stat.h>
 #include <fcntl.h>
+#include <unistd.h>
+#include <stdio.h>
+#include <stdlib.h>
 
 int _putchar(char c);
 ssize_t read_textfile(const char *filename, size_t letters);
@@ -12,5 +15,15 @@ int create_file(const char *filename, char *text_content);
 int append_text_to_file(const char *filename, char *text_content);
 char *create_buffer(char *file);
 void close_file(int fd);
+void check_elf(unsigned char *e_ident);
+void print_magic(unsigned char *e_ident);
+void print_class(unsigned char *e_ident);
+void print_data(unsigned char *e_ident);
+void print_version(unsigned char *e_ident);
+void print_abi(unsigned char *e_ident);
+void print_osabi(unsigned char *e_ident);
+void print_type(unsigned int e_type, unsigned char *e_ident);
+void print_entry(unsigned long int e_entry, unsigned char *e_ident);
+void close_elf(int elf);
 
 #endif
