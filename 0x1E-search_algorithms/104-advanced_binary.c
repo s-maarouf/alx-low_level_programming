80feb49952e45b57912b60c729c55981f7258bd0
s-maarouf <maarouf.salahedd@gmail.com>
2023-10-14 21:40:10 +0100

    Completed task 11

--- 0x1E-search_algorithms/104-advanced_binary.c
+++ 0x1E-search_algorithms/104-advanced_binary.c
@@ -1,6 +1,49 @@
 #include "search_algos.h"
 
 /**
+ * rec_search - searches for a value in an array of
+ * integers using the Binary search algorithm
+ *
+ *
+ * @array: input array
+ * @size: size of the array
+ * @value: value to search in
+ * Return: index of the number
+ */
+
+int rec_search(int *array, size_t size, int value)
+{
+	size_t half = size / 2;
+	size_t i;
+
+	if (array == NULL || size == 0)
+		return (-1);
+
+	printf("Searching in array");
+
+	for (i = 0; i < size; i++)
+		printf("%s %d", (i == 0) ? ":" : ",", array[i]);
+
+	printf("\n");
+
+	if (half && size % 2 == 0)
+		half--;
+
+	if (value == array[half])
+	{
+		if (half > 0)
+			return (rec_search(array, half + 1, value));
+		return ((int)half);
+	}
+
+	if (value < array[half])
+		return (rec_search(array, half + 1, value));
+
+	half++;
+	return (rec_search(array + half, size - half, value) + half);
+}
+
+/**
  * advanced_binary - searches for a value in a sorted array of integers
  * using the Binary search algorithm
  *
@@ -13,35 +56,12 @@
 
 int advanced_binary(int *array, size_t size, int value)
 {
-	size_t i, mid, idx = 0;
-	int tmp;
+	int index;
 
-	if (!array)
+	index = rec_search(array, size, value);
+
+	if (index >= 0 && array[index] != value)
 		return (-1);
 
-	printf("Searching in array: ");
-	for (i = 0; i < size; i++)
-		printf("%d%s", array[i], i < size - 1 ? ", " : "\n");
-
-	if (size == 1 && array[0] != value)
-		return (-1);
-
-	mid = (size - 1) / 2;
-	if (array[mid] == value)
-	{
-		if (array[mid - 1] < value)
-			return (mid);
-	}
-	if (array[mid] < value)
-	{
-		idx += mid + 1;
-		array += mid + 1;
-		if (size % 2 != 0)
-			mid--;
-	}
-	mid++;
-	tmp = advanced_binary(array, mid, value);
-	if (tmp != -1)
-		return (tmp + idx);
-	return (-1);
+	return (index);
 }
